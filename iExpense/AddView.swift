ece87522671057deed564c329d307b22f4fa32a4
Razvan Pricop <razvanipricop@gmail.com>
2024-10-21 20:01:15 +0200

    Initial commit
    First commit of the repo with basic UI and logic

---
+++ iExpense/AddView.swift
@@ -0,0 +1,46 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Razvan Pricop on 21.10.24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    @Environment(\.dismiss) var dismiss
+    
+    let types = ["Business", "Personal"]
+    var expenses: Expenses
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) { type in
+                        Text(type)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: "EUR"))
+            }
+            .navigationTitle("Add Expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "EUR"))
+                        
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("Expenses")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    showingAddExpense.toggle()
+                }
+            }
         }
-        .padding()
+        .sheet(isPresented: $showingAddExpense) {
+            AddView(expenses: expenses)
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
